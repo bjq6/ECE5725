43944541563d961b2c6df7329b87b606217938d1
Nikita Ermoshkin <ne75@cornell.edu>
2017-04-03 11:28:25 -0400

    setting up a bare metal project. needs more work still, but example will compile and run on RPI2

---
+++ final_project/rpi_test_project_broken/scripts/CMakeFiles/rpi_test_project.dir/DependInfo.cmake
@@ -0,0 +1,45 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/start.S" "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/scripts/CMakeFiles/rpi_test_project.dir/start.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "RPI2=1"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/cstartup.c" "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/scripts/CMakeFiles/rpi_test_project.dir/cstartup.c.obj"
+  "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/cstubs.c" "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/scripts/CMakeFiles/rpi_test_project.dir/cstubs.c.obj"
+  "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/main.c" "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/scripts/CMakeFiles/rpi_test_project.dir/main.c.obj"
+  "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/rpi-armtimer.c" "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/scripts/CMakeFiles/rpi_test_project.dir/rpi-armtimer.c.obj"
+  "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/rpi-gpio.c" "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/scripts/CMakeFiles/rpi_test_project.dir/rpi-gpio.c.obj"
+  "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/rpi-interrupts.c" "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/scripts/CMakeFiles/rpi_test_project.dir/rpi-interrupts.c.obj"
+  "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/rpi-systimer.c" "/Users/Nikita/Github/ECE5725/final_project/rpi_test_project/scripts/CMakeFiles/rpi_test_project.dir/rpi-systimer.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "RPI2=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ final_project/rpi_test_project_broken/scripts/CMakeFiles/rpi_test_project.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rpi_test_project.dir/main.c.obj"
+  "CMakeFiles/rpi_test_project.dir/cstartup.c.obj"
+  "CMakeFiles/rpi_test_project.dir/cstubs.c.obj"
+  "CMakeFiles/rpi_test_project.dir/start.S.obj"
+  "CMakeFiles/rpi_test_project.dir/rpi-armtimer.c.obj"
+  "CMakeFiles/rpi_test_project.dir/rpi-gpio.c.obj"
+  "CMakeFiles/rpi_test_project.dir/rpi-interrupts.c.obj"
+  "CMakeFiles/rpi_test_project.dir/rpi-systimer.c.obj"
+  "rpi_test_project.pdb"
+  "rpi_test_project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/rpi_test_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
